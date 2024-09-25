536a2d0bb832163382288f0c8302fbbca64eb005
default <default@NITRO>
2024-09-25 11:55:19 +0300

    Add project files.

---
+++ c#/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+//Клас відстеження помилок, який повинна існувати в єдиному екземплярі. Реалізовувати методи:
+//1) Фіксування помилки(Час, код, текст з описом помилки)
+//2) Очищення історії помилок.
+//3) Вивід інформації про всі помилки
+//4) Збереження історії помилок до файлу.
+namespace C_.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var errors = ErrorHandler.Instance;
+            errors.errorLog("someCode", "404Error");
+            errors.errorLog("cod2", "Error");
+            errors.showDataErrors();
+            errors.errortoFile();
+        }
+    }
+    public class ErrorHandler
+    {
+        private static ErrorHandler _instance;
+        private List<string> _errorLoglist = new List<string>();
+
+        public ErrorHandler() { }
+
+        public static ErrorHandler
+        Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ErrorHandler();
+                }
+                return _instance;
+
+            }
+        }
+        public void errorLog(string data, string errormessage)
+        {
+            string error = $"Data/Time: {DateTime.Now},Code: {data}, Error: {errormessage}";
+            _errorLoglist.Add(error);
+        }
+        public void clearErrorData()
+        {
+            _errorLoglist.Clear();
+        }
+        public void showDataErrors()
+        {
+            foreach (string error in _errorLoglist)
+            {
+                Console.WriteLine(error);
+            }
+        }
+        public void errortoFile()
+        {
+            for (int i = 0; i < _errorLoglist.Count; i++)
+            {
+                _errorLoglist[i].ToString();
+            }
+            File.WriteAllLines("errors.txt", _errorLoglist);
+            Console.WriteLine("Error list has been written to file:'errors.txt'");
+        }
+    }
+}
+
---
+++ c#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("c#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("c#")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1d91983-df7a-45b0-8c30-095119766c2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
